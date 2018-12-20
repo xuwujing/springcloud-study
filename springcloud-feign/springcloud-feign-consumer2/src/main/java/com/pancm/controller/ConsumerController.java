9482fe6fc3821c27bfa29d9f80541b0e2c80b308
xuwujing <1060589146@qq.com>
2018-12-20 18:01:45 +0800

    fegin项目提交

---
+++ springcloud-feign/springcloud-feign-consumer2/src/main/java/com/pancm/controller/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.pancm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+* @Title: ConsumerController
+* @Description: 
+* 用于做转发请求
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年3月29日
+ */
+@RestController
+public class ConsumerController {
+
+	@RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return name+",Hello World";
+    }
+
+}
