3d84095b69595b0d227531a428757948c22e56a5
xuwujing <1060589146@qq.com>
2018-12-21 14:46:41 +0800

    fegin项目工程提交

---
+++ springcloud-feign/springcloud-feign-consumer/src/main/java/com/pancm/FeignConsumerApplication.java
@@ -0,0 +1,27 @@
+package com.pancm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 
+* @Title: Application
+* @Description:
+* 程序入口 
+* EnableDiscoveryClient: 启用服务注册与发现
+  EnableFeignClients:    启用feign进行远程调用
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年3月29日
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class FeignConsumerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FeignConsumerApplication.class, args);
+		  System.out.println("feign第一个消费者服务启动...");
+	}
+}
