5225647b7e9e95903c8574bd9d283af5abd5ab20
xuwujing <1060589146@qq.com>
2018-12-21 16:53:40 +0800

    fegin项目工程提交

--- springcloud-feign/springcloud-feign-eureka/src/main/java/com/pancm/FeignEurekaApplication.java
+++ springcloud-feign/springcloud-feign-eureka/src/main/java/com/pancm/FeignEurekaApplication.java
@@ -16,9 +16,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class FeignEurekaApplication {
   public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
-      System.out.println("注册中心服务启动...");
+      SpringApplication.run(FeignEurekaApplication.class, args);
+      System.out.println("feign注册中心服务启动...");
   }
 }
