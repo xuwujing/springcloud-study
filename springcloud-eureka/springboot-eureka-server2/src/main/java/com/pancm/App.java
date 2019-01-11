4abcd5f626e39adf04ffbfab890d89425807e110
xuwujing <1060589146@qq.com>
2019-01-11 17:38:04 +0800

    高可用注册中心代码示例

---
+++ springcloud-eureka/springboot-eureka-server2/src/main/java/com/pancm/App.java
@@ -0,0 +1,23 @@
+package com.pancm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+* @Title: Application
+* @Description: 
+* springcloud服务入口
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年3月29日
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+      SpringApplication.run(App.class, args);
+      System.out.println("第二个注册中心服务启动...");
+  }
+}
