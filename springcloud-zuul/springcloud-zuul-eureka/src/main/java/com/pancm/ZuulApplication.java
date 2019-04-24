eaa32c894bbaff269beb8fc036ff5b0a7f37f96a
xuwujing <1060589146@qq.com>
2019-04-24 19:39:56 +0800

    增加zuul路由网关工程示例

---
+++ springcloud-zuul/springcloud-zuul-eureka/src/main/java/com/pancm/ZuulApplication.java
@@ -0,0 +1,24 @@
+package com.pancm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+* @Title: Application
+* @Description: 
+* springcloud服务入口
+  EnableEurekaServer 注解启动一个服务注册中心
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年12月21日
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class ZuulApplication {
+  public static void main(String[] args) {
+      SpringApplication.run(ZuulApplication.class, args);
+      System.out.println("zuul注册中心服务启动...");
+  }
+}
